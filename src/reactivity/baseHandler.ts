509046f9fe4f32ca41e1f23c9b1a0d0deba6eb1a
cal <po6271933@outlook.com>
2023-10-05 22:34:50 +0800

    first commit

---
+++ src/reactivity/baseHandler.ts
@@ -0,0 +1,49 @@
+import { track, trigger } from "./effect";
+import { ReactiveFlags, reactive, readonly } from "./reactive";
+import { isObject } from "../share";
+const get = createGetter()
+const set = createSetter()
+const readonlyGet = createGetter(true)
+
+function createGetter(isReadonly = false) {
+  return function get(target, key) {
+
+    if(key === ReactiveFlags.IS_REACTIVE) {
+      return !isReadonly;
+    } else if(key === ReactiveFlags.IS_READONLY) {
+      return isReadonly;
+    }
+
+    const res = Reflect.get(target, key);
+
+    if(isObject(res)) {
+      return isReadonly ? readonly(res) : reactive(res)
+    }
+    if(!isReadonly) {
+      track(target, key);
+    }
+    return res;
+  }
+}
+
+function createSetter() {
+  return function set(target, key, value) {
+    const res = Reflect.set(target, key, value);
+    trigger(target, key);
+    return res;
+  }
+}
+
+
+export const mutableHandlers = {
+  get,
+  set
+}
+
+export const readonlyHandlers = {
+  get: readonlyGet,
+  set(target, key, value) {
+    console.warn(`key: ${key} set 失败 因为 target 是 readonly`);
+    return true;
+  }
+}
